224579e11e60dea8ec704b7bb4292ecdc6b26488
Ioannidis <panos277@hotmail.com>
2020-02-19 01:30:20 +0200

    Fix HomeHelp back button

--- app/src/main/java/com/papei/instantservice/HomeHelpActivity.java
+++ app/src/main/java/com/papei/instantservice/HomeHelpActivity.java
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class HomeHelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_help);
+        setTitle(R.string.title_activity_help);
+
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setDefaultDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    // Add functionality to back button
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
