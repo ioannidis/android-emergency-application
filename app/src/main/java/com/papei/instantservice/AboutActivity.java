356d395676c7371c5ed926079e79e75c0a86338b
Ioannidis <panos277@hotmail.com>
2020-02-20 23:13:54 +0200

    Remove unused imports

--- app/src/main/java/com/papei/instantservice/AboutActivity.java
+++ app/src/main/java/com/papei/instantservice/AboutActivity.java
@@ -3,16 +3,9 @@ package com.papei.instantservice;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.MenuItem;
-
-import java.util.Locale;
 
 public class AboutActivity extends AppCompatActivity {
 
