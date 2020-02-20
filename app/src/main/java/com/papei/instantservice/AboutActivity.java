6373e66c805c4828fc965eb4597da50753e8f96f
Ioannidis <panos277@hotmail.com>
2020-02-20 23:12:42 +0200

    Fix about title when change lang

--- app/src/main/java/com/papei/instantservice/AboutActivity.java
+++ app/src/main/java/com/papei/instantservice/AboutActivity.java
@@ -3,9 +3,16 @@ package com.papei.instantservice;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import java.util.Locale;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -13,6 +20,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setTitle(R.string.about);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
